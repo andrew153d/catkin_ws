de7a9ba45b466def4e6f2240719c4ae5fdbec273
andrew153d <andrewwisnergrow@gmail.com>
2023-01-23 19:41:58 -0500

    filters cloud by z value

--- src/point_cloud_processor/src/point_cloud_node.cpp
+++ src/point_cloud_processor/src/point_cloud_node.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/passthrough.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/filter.h>
@@ -8,44 +9,31 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+ros::Publisher pub;
+
 void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
+// Convert the ROS message to PCL point cloud
+    pcl::PointCloud<pcl::PointXYZRGBA> cloud;
+    pcl::fromROSMsg(*msg, cloud);
 
-  // Convert the ROS message to a PCL point cloud
-  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
-  pcl::fromROSMsg(*msg, *cloud);
-  std::vector<int> indices;
-  pcl::removeNaNFromPointCloud(*cloud, *cloud, indices);
-  
-  //printf("\nPoint 0: X: %f, Y: %f, Z: %F, R: %lf", cloud->points[0].x, cloud->points[0].y, cloud->points[0].z);
-  for(auto point: cloud->points){
-    std::cout << point << std::endl;
-  }
-  //std::cout << cloud->points[0] << std::endl;
-   //std::cout << cloud->points[0];
+    // Remove NaN points
+    std::vector<int> indices;
+    pcl::removeNaNFromPointCloud(cloud, cloud, indices);
 
-  //for (int i = 0; i < msg->fields.size(); i++)
-  //{
-    //std::cout << msg->fields[i] << std::endl;
-  //}
-  //const float *cloudData = reinterpret_cast<const float *>(&msg->data[0]);
-  //int counter = 0;
+    // Filter points based on height
+    pcl::PassThrough<pcl::PointXYZRGBA> pass;
+    pass.setInputCloud (cloud.makeShared());
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (-0.3, 0.3);
+    pass.filter (cloud);
 
-  //std::cout << cloudData[0];
-
-  /*for(int i = 0; i< msg->data.size(); i++){
-    uint32_t p = msg->data[i];
-    if(counter>10){
-      break;
-    }
-    if(!isnan(p)){
-      std::cout << "x: " << p << std::endl;
-      counter++;
-    }
-  }*/
-
-  // Print the number of points in the point cloud
-  // std::cout << "Point cloud has " << cloud->points.size() << " poistd:nts." << :endl;
+    // Convert the filtered point cloud to ROS message
+    sensor_msgs::PointCloud2 filtered_msg;
+    pcl::toROSMsg(cloud, filtered_msg);
+    filtered_msg.header = msg->header;
+  pub.publish(filtered_msg);
 }
 
 int main(int argc, char **argv)
@@ -53,6 +41,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "point_cloud_node");
   ros::NodeHandle nh;
 
+  pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
   ros::Subscriber cloud_sub = nh.subscribe("/zed2/zed_node/point_cloud/cloud_registered", 1, cloud_callback);
 
   ros::spin();
